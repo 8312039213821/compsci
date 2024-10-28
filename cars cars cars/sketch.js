64973af6547ef17bceaaefd5fa78aea75af81234
8312039213821 <0302415@spsd.sk.ca>
2024-10-28 15:05:53 -0600

    update to keep things updated, oct 30

--- cars cars cars/sketch.js
+++ cars cars cars/sketch.js
@@ -1,80 +1,170 @@
-// cars cars cars
+//cars cars cars
+//Sami
+//Oct 23, 2024
 
-let myVehicle;
+let eastBound = [];
+let westBound = [];
+let trafficLight;
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  myVehicle = new Vehicle;
+ 
+  // adding vehicles
+  for (let i = 0; i < 20; i++) {
+    eastBound.push(new Vehicle(1));
+    westBound.push(new Vehicle(0));
+  }
+
+
+  trafficLight = new TrafficLight();
 }
+
 
 function draw() {
   background(0, 255, 120);
   drawRoad();
+ 
+  // eastbound vehicles
+  for (let vehicle of eastBound) {
+    vehicle.action();
+  }
+
+
+  // westbound vehicles
+  for (let vehicle of westBound) {
+    vehicle.action();
+  }
+
+
+  trafficLight.display();
+  trafficLight.checkState();
 }
+
 
 function drawRoad(){
   rectMode(CENTER);
-  fill(0)
-  rect(width/2, height/2, width, height/2);
-  for (let i = 0; i <= width; i += width/12) {
-    fill(255, 255, 0);
-    rect(i, height/2, width/24, 12);
+  fill(0);
+  rect(width / 2, height / 2, width, height / 2);
+  for (let i = 0; i <= width; i += width / 12) {
+    fill(255, 255, 255);
+    rect(i, height / 2, width / 24, 12);
   }
 }
 
-class Vehicle{
-  constructor(x, y, type, color, direction, speed){
-    this.x = x;
-    this.y = y;
-    this.type = type;
-    this.color = color;
+
+class Vehicle {
+  constructor(direction) {
+    this.type = int(random(0, 2));
+    this.color = color(random(255), random(255), random(255));
+    this.y = random(height / 4, 3 * height / 4);
     this.direction = direction;
-    this.speed = speed;
+    this.xSpeed = this.direction === 1 ? random(2, 5) : random(-5, -2);
+    this.x = this.direction === 1 ? 0 : width;
   }
 
 
-  display(){
-    if(type === 0){
-
+  display() {
+    fill(this.color);
+    if (this.type === 0) {
+      fill(this.color);
+      rect(this.x, this.y, 50, 25); // Car
+      fill(255, 255, 255);
+      rect(this.x + 15, this.y + 15, 20, 5);     
+      rect(this.x - 15, this.y + 15, 20, 5);   
+      rect(this.x + 15, this.y - 15, 20, 5);   
+      rect(this.x - 15, this.y - 15, 20, 5);    
+    } else {
+      rect(this.x, this.y, 75, 35); // Truck
     }
-    else{
-
-    }
-
   }
 
 
-  move(){
-    this.x += this.speed;
-  }    
-
-  speedUp(){
-      this.speed += 4;
+  move() {
+    if (!trafficLight.red) {
+      this.x += this.xSpeed;
+      if (this.x > width + 50) {
+        this.x = -50;
+      } else if (this.x < -50) {
+        this.x = width + 50;
+      }
+    }
   }
 
-  speedDown(){
-      this.speed -= 4;
+
+  speedUp() {
+    if (abs(this.xSpeed) < 15) {
+      this.xSpeed *= 1.1;
+    }
   }
 
-  changeColor(){
-      this.color = random((0, 0, 0),(255, 255, 255));
-  }   
 
-  action(){
-    let a = random(0, 100);
-    let b = random(0, 100);
-    let c = random(0, 100);
-
-    if(a === 3){
-
+  speedDown() {
+    if (abs(this.xSpeed) > 1) {
+      this.xSpeed *= 0.9;
     }
-    if(b === 3){
-      
-    }
-    if(c === 3){
-      
-    }
-  }  
+  }
 
+
+  changeColor() {
+    this.color = color(random(255), random(255), random(255));
+  }
+
+
+  action() {
+    this.move();
+    if (random(100) < 1) {
+      this.speedUp();
+    }
+    if (random(100) < 1) {
+      this.speedDown();
+    }
+    if (random(100) < 1) {
+      this.changeColor();
+    }
+    this.display();
+  }
+}
+
+
+class TrafficLight {
+  constructor() {
+    this.state = "green";
+    this.red = false;
+    this.timer = 0;
+  }
+
+
+  display() {
+    fill(this.red ? 'red' : 'green');
+    rect(width - 50, 50, 20, 60);
+  }
+
+
+  checkState() {
+    if (this.red) {
+      this.timer++;
+      if (this.timer > 120) {
+        this.red = false;
+        this.timer = 0;
+      }
+    }
+  }
+}
+
+
+function keyPressed() {
+  if (key === ' ') {
+    trafficLight.red = true;
+  }
+}
+
+
+function mousePressed() {
+  if (mouseButton === LEFT && !keyIsDown(SHIFT)) {
+    eastBound.push(new Vehicle(1));
+  } else if (mouseButton === LEFT && keyIsDown(SHIFT)) {
+    westBound.push(new Vehicle(0));
+  }
 }
 
---
+++ puzzle start/sketch.js
@@ -0,0 +1,33 @@
+// puzzle start
+// Sami
+// Oct 30, 2024
+let grid = [[0, 100, 250, 50, 200], 
+            [255,0, 150, 100, 255],
+            [0, 0, 255, 100, 50]];
+
+const NUM_ROWS = 3;
+const NUM_COLS = 5;
+
+let rectheight, rectwidth;
+
+function getArrayY(){
+  return(int(mouseY/rectheight))
+}
+
+function getArrayX(){
+  return(int(mouseX/rectwidth))
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  
+}
+
+function draw() {
+  background(220);
+  for (let x = 0; x < NUM_ROWS; x++) {
+    for (let y = 0; y < NUM_COLS; y++) {
+      element = grid[index];
+    }    
+  }
+}
